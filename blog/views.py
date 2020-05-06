788040432c707e5065433ebc64f3b99f781c8705
tanabe_taichi <whpbj809@gmail.com>
2020-05-07 00:28:08 +0900

    画面構成軽く書き起こし+テンプレートビューを試しに適用

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,14 @@
-from django.shortcuts import render
+from django.views import generic
 from .models.article import Article
 
 
-def index(request):
-    article_list = Article.objects.filter(status=Article.STATUS_MAP['PUBLIC']).order_by('-created_dt')[:3]
-    context = {
-        'article_list': article_list,
-    }
-    return render(request, 'blog/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'blog/index.html'
 
-def detail(request, article_id):
-    article = Article.objects.get(pk=article_id)
-    context = {
-        'article': article,
-    }
-    return render(request, 'blog/detail.html', context)
+    def get_queryset(self):
+        return Article.objects.filter(status=Article.STATUS_MAP['PUBLIC']).order_by('-created_dt')[:3]
+
+
+class DetailView(generic.DetailView):
+    model = Article
+    queryset = Article.objects.filter(status=Article.STATUS_MAP['PUBLIC'])

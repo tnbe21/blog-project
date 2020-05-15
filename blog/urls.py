84154cea32b69cc806ad0f460735c9b7c756e0cb
tanabe_taichi <whpbj809@gmail.com>
2020-05-16 01:22:06 +0900

    URLパターンとテンプレ追加

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,47 @@
-from django.urls import path
+from django.urls import path, register_converter
 
 from . import views
+
+class FourDigitYearConverter:
+    regex = '[0-9]{4}'
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return '%04d' % value
+
+
+class MonthConverter:
+    regex = '[0-9]{1,2}'
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return value
+
+
+class PageNumConverter:
+    regex = '\d*'
+
+    def to_python(self, value):
+        return int(value) if value != '' else 1;
+
+    def to_url(self, value):
+        return value
+
+
+register_converter(FourDigitYearConverter, 'yyyy')
+register_converter(MonthConverter, 'mm')
+register_converter(PageNumConverter, 'page')
 
 app_name = 'blog'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('index/<int:page_num>', views.IndexView.as_view(), name='index'),
+    path('month/<yyyy:year>/<mm:month>/<page:page_num>', views.MonthListView.as_view(), name='month_list'),
+    path('tag/<slug:tag>/<page:page_num>', views.TagListView.as_view(), name='tag_list'),
+    path('search', views.SearchResultListView.as_view(), name='search_result_list'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,75 @@ from django.views import generic
 from .models.article import Article
 
 
-class IndexView(generic.ListView):
+LMT = 3
+
+class BaseView(generic.View):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # URLパラメータをすべてレスポンスのコンテキスト内に詰める
+        context.update(self.kwargs)
+        # 年月ごとの記事数のリスト
+        # タグとそれに紐づく記事数のリスト
+        return context
+
+
+class IndexView(BaseView, generic.ListView):
     template_name = 'blog/index.html'
 
     def get_queryset(self):
+        page_num = self.kwargs.get('page_num', 1)
+        start = LMT * (page_num - 1)
+        return Article.objects.filter(status=Article.STATUS_MAP['PUBLIC']).order_by('-created_dt')[start:(start + LMT)]
+
+
+class MonthListView(BaseView, generic.ListView):
+    template_name = 'blog/month_list.html'
+
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        page_num = self.kwargs.get('page_num', 1)
+        start = LMT * (page_num - 1)
+        return Article.objects.filter(status=Article.STATUS_MAP['PUBLIC']).order_by('-created_dt')[start:(start + LMT)]
+
+
+class TagListView(BaseView, generic.ListView):
+    template_name = 'blog/tag_list.html'
+
+    def get_queryset(self):
+        tag = self.kwargs.get('tag')
+        page_num = self.kwargs.get('page_num', 1)
+        start = LMT * (page_num - 1)
+        return Article.objects.filter(status=Article.STATUS_MAP['PUBLIC']).order_by('-created_dt')[start:(start + LMT)]
+
+
+class SearchResultListView(BaseView, generic.ListView):
+    template_name = 'blog/search_result_list.html'
+
+    params = {}
+
+    def __get_params(self):
+        if not self.params:
+            query_params = self.request.GET
+            self.params = {
+                'word': query_params.get('w'),
+                'page_num': query_params.get('p', 1),
+            }
+
+        return self.params
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(self.__get_params())
+        return context
+
+    def get_queryset(self):
+        word, page_num = self.__get_params().values()
+        print("word: {}".format(word))
+        print("page_num: {}".format(page_num))
         return Article.objects.filter(status=Article.STATUS_MAP['PUBLIC']).order_by('-created_dt')[:3]
 
 
-class DetailView(generic.DetailView):
+class DetailView(BaseView, generic.DetailView):
     model = Article
     queryset = Article.objects.filter(status=Article.STATUS_MAP['PUBLIC'])
